5415d1971b3b2d213d25a152bec37d51f9a4cda9
20 Centruy Boy <110159240+20CenturyBoy@users.noreply.github.com>
2023-10-10 14:56:59 +0200

    Adds button which generates sketchpad
    Given commit creates button on webpage via which its possible to
    generate new sketchpad on web page in place of old dynamically.
    Due to it(button) being defined to have  click event listener with event
    handler which removes previously defined sketchpad and creates new one
    based on inputed number via prompt function.Esentally given the user
    ability to via given button create new sketchpad and to determine number
    of squares sketchpad should contain.
    
    And in order for said sketchpad to have same size as previous one
    squares are now insted of having consistent size set to have size based on
    container size/avialiable space and number of other squares in it due to
    each needing to have equal amount of space assigned to it in container/whole.

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-let n = 16;
 
-for(let i=0;i<n;i++)
+function generateSketchpad(n)
 {
-    let content = document.querySelector('#content');
-    let container = document.createElement('div');
-    for(let j=0;j<n;j++)
+    for(let i=0;i<n;i++)
     {
-        const div = document.createElement('div');
-        div.classList.add('new');
-        div.addEventListener('mouseover',() => {
-            div.style.backgroundColor= 'black';
-        })                      
-        container.appendChild(div);
+        let content = document.querySelector('#content');
+        let container = document.createElement('div');
+        container.classList.add('container');
+        for(let j=0;j<n;j++)
+        {
+            const div = document.createElement('div');
+            div.classList.add('new');
+            div.addEventListener('mouseover',() => {
+                div.style.backgroundColor= 'black';
+            })                      
+            container.appendChild(div);
+        }
+        content.appendChild(container);
     }
-    content.appendChild(container);
 }
+
+let n = 16  ;
+generateSketchpad(n);
+
+const button = document.querySelector('#button');
+button.addEventListener('click',() => {
+    const contentContainer = document.querySelector('#content-container');
+    let content = document.querySelector('#content');
+    contentContainer.removeChild(content);
+    content = document.createElement('div');
+    content.setAttribute('id', 'content');
+    contentContainer.appendChild(content);
+    let n = prompt("How many squares would you like in new sketchpad?")
+    generateSketchpad(n);
+
+})
+
